f25d865077d378f910416acb283a69851d4ae184
volkangurbuz <volkangurbuz16@gmail.com>
2022-07-08 23:16:11 +0200

    Initial commit

---
+++ src/main/java/dev/volkangurbuz/workschedule/WorkScheduleApplication.java
@@ -0,0 +1,13 @@
+package dev.volkangurbuz.workschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkScheduleApplication.class, args);
+    }
+
+}
