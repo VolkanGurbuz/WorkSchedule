fb59e9a4577955a3c2ddd1a361a5a0f4c261fc82
volkangurbuz <volkangurbuz16@gmail.com>
2022-10-31 21:25:57 +0100

    exception handled with map

--- src/main/java/dev/volkangurbuz/workschedule/exceptions/GlobalExceptionHandler.java
+++ src/main/java/dev/volkangurbuz/workschedule/exceptions/GlobalExceptionHandler.java
@@ -7,20 +7,33 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(EntityNotFoundException.class)
   @ResponseStatus(HttpStatus.NOT_FOUND)
-  public final ResponseEntity<String> entity(
+  public final ResponseEntity<Object> entity(
       EntityNotFoundException exception, WebRequest request) {
-    return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("timestamp", LocalDateTime.now());
+    body.put("message", "No config found");
+
+    return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler(RuntimeException.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public ResponseEntity<Object> handleAllUncaughtException(
       RuntimeException exception, WebRequest request) {
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Please check your query.");
+    Map<String, Object> body = new LinkedHashMap<>();
+    body.put("timestamp", LocalDateTime.now());
+    body.put("message", "No config found");
+
+    return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+
   }
 }
