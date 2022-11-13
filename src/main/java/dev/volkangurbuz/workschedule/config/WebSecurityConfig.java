b42ea780d103bc0623db1b13cc7518cdfa85685c
volkangurbuz <volkangurbuz16@gmail.com>
2022-11-13 20:42:34 +0100

    injected directly to the method

--- src/main/java/dev/volkangurbuz/workschedule/config/WebSecurityConfig.java
+++ src/main/java/dev/volkangurbuz/workschedule/config/WebSecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,8 +27,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebSecurityConfig {
   @Autowired WorkerServiceImpl userDetailsService;
 
-  @Autowired private AuthEntryPointJwt unauthorizedHandler;
-
   @Bean
   public AuthTokenFilter authenticationJwtTokenFilter() {
     return new AuthTokenFilter();
@@ -43,7 +40,7 @@ public class WebSecurityConfig {
         .csrf()
         .disable()
         .exceptionHandling()
-        .authenticationEntryPoint(unauthorizedHandler)
+        .authenticationEntryPoint(new AuthEntryPointJwt())
         .and()
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
